20a4404972e62e12613ec40ac31fc1f91ea92530
P N <petyanikleva@yahoo.com>
2024-03-02 21:01:58 +0200

    add hero banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import HeroBanner from '../components/HeroBanner.jsx'
+import SearchExercises from '../components/SearchExercises.jsx'
+import Exercises from '../components/Exercises.jsx'
+import { Box } from '@mui/material'
 
 const Home = () => {
+   
   return (
-    <div>Home</div>
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
   )
 }
 
