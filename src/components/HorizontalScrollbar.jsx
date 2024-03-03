b706c5a0b123ddd6a415c304b5b5c343730f466c
P N <petyanikleva@yahoo.com>
2024-03-03 17:13:44 +0200

    fix horizontalscroll

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,10 +1,10 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/right-arrow.png';
 import LeftArrowIcon from '../assets/left-arrow.png';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
@@ -26,16 +26,16 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
     {data.map((item) => (
       <Box
         key={item.id || item}
-        itemID={item.id || item}
+        itemId={item.id || item}
         title={item.id || item}
         m="0 40px"
       >
-        {<BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> }
+        <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> 
       </Box>
     ))}
   </ScrollMenu>
