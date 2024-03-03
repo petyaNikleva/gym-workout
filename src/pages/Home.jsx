25d06b4d686ce893388ae58f306fc05b45e769f9
P N <petyanikleva@yahoo.com>
2024-03-03 17:04:22 +0200

    add searchExercises, bodypart, horizontalscroll comonents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeroBanner from '../components/HeroBanner.jsx'
 import SearchExercises from '../components/SearchExercises.jsx'
 import Exercises from '../components/Exercises.jsx'
 import { Box } from '@mui/material'
 
 const Home = () => {
-   
+  const [exercise, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
-        <HeroBanner />
-        <SearchExercises />
-        <Exercises />
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        serBodyPart={setBodyPart} />
     </Box>
   )
 }
