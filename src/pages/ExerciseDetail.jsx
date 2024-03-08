8651ec2a3a24f3838659556acd2f7a8e27f06d88
P N <petyanikleva@yahoo.com>
2024-03-08 16:24:54 +0200

    add deatail component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import { useEffect, useState } from 'react'
+import ExerciseVideos from '../components/ExerciseVideos.jsx'
+import SimilarExercises from '../components/SimilarExercises.jsx'
+import Detail from '../components/Detail.jsx'
+import { useParams } from 'react-router-dom'
+import { exerciseOptions, fetchData } from '../utils/fetchData.js'
+import { BASE_URL } from '../utils/constants.js'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const youTubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
+      const exerciseDetailData = await fetchData(`${BASE_URL}/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData); 
+    
+    }
+    fetchExercisesData(); 
+
+  },[id])
   
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
